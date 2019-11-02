f9611323c98f7c14e9c3d62cffc8426105929b70
Abdullah Alhaider <cs.alhaider@gmail.com>
2019-11-02 23:56:23 +0300

    Initial Commit

---
+++ Previewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Previewer
+//
+//  Created by Abdullah Alhaider on 11/2/19.
+//  Copyright © 2019 Abdullah Alhaider. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
